cafeeb01918c5cf1bf7934c8948d3758100e2ac4
Annabelle Brown <amb2528@nau.edu>
2021-10-20 08:42:23 -0700

    format/transformers

--- q2_autopepsirf/format_types.py
+++ q2_autopepsirf/format_types.py
@@ -1,7 +1,26 @@
-import os
-
 import qiime2.plugin.model as model
 from qiime2.plugin import SemanticType
+
 from q2_types.feature_table import FeatureTable
 
-rawCounts = SemanticType('rawCounts', variant_of=FeatureTable.field['content'])
+
+Normed = SemanticType('Normed', variant_of=FeatureTable.field['content'])
+NormedDifference = SemanticType('NormedDifference', variant_of=FeatureTable.field['content'])
+NormedDiffRatio = SemanticType('NormedDiffRatio', variant_of=FeatureTable.field['content'])
+Zscore = SemanticType('Zscore', variant_of=FeatureTable.field['content'])
+RawCounts = SemanticType('RawCounts', variant_of=FeatureTable.field['content'])
+
+
+class PepsirfContingencyTSVFormat(model.TextFileFormat):
+    def _validate_(self, level='min'):
+        with self.open() as fh:
+            for _, line in zip(range(1), fh):
+                if not line.startswith('Sequence name\t'):
+                    raise model.ValidationError(
+                        'TSV does not start with "Sequence name"')
+
+
+PepsirfContingencyTSVDirFmt = model.SingleFileDirectoryFormat(
+    'PepsirfContingencyTSVDirFmt',
+    'pepsirf-table.tsv',
+    PepsirfContingencyTSVFormat)
