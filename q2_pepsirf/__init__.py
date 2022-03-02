1cd0c70ce2eb2653f60eca59dc10ab45e8dae8e9
Annabelle Brown <amb2528@nau.edu>
2022-03-02 09:33:52 -0700

    add new modules to init

--- q2_pepsirf/__init__.py
+++ q2_pepsirf/__init__.py
@@ -3,7 +3,11 @@ from q2_pepsirf.actions.info import infoSumOfProbes, infoSNPN
 from q2_pepsirf.actions.enrich import enrich
 from q2_pepsirf.actions.zscore import zscore
 from q2_pepsirf.actions.bin import bin
+from q2_pepsirf.actions.deconv import deconv_batch, deconv_singluar
+from q2_pepsirf.actions.demux import demux
+from q2_pepsirf.actions.link import link
+from q2_pepsirf.actions.subjoin import subjoin
 
-__all__ = ['norm', 'infoSumOfProbes', 'infoSNPN', 'enrich', 'zscore', 'bin']
+__all__ = ['norm', 'infoSumOfProbes', 'infoSNPN', 'enrich', 'zscore', 'bin', 'deconv_batch', 'deconv_singluar', 'demux', 'link', 'subjoin']
 from . import _version
 __version__ = _version.get_versions()['version']
